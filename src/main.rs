f185abbc3f12cfa999a2e0d80d3f297aa5b1be6a
Romeo923 <cromeo5112@gmail.com>
2024-09-15 21:31:25 -0400

    Play songs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 mod cli;
+mod player;
 mod scanner;
-
-use cli::Command;
 
 fn main() {
     let args = cli::parse_args();
     match args.command {
-        Command::Scan { directory } => {
+        cli::Command::Scan { directory } => {
             println!("Scanning {directory}...");
             scanner::scan_directory(directory)
-        },
-        Command::List => println!("Listing Songs"),
-        Command::Play { file } => println!("Now Playing {file}..."),
+        }
+        cli::Command::List => println!("Listing Songs"),
+        cli::Command::Play { file } => player::play_song(file),
     }
 }
