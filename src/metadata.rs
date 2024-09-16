bf2d6cb702a3b5b1e3fc4da586384190c37c33f0
Romeo923 <cromeo5112@gmail.com>
2024-09-15 22:45:57 -0400

    Use file name is title does not exist in tag

--- src/metadata.rs
+++ src/metadata.rs
@@ -1,5 +1,6 @@
 use id3::{Tag, TagLike};
 use serde::{Deserialize, Serialize};
+use std::path::Path;
 
 #[derive(Serialize, Deserialize)]
 pub struct MusicFile {
@@ -12,9 +13,14 @@ pub struct MusicFile {
 
 pub fn read_metadata(path: &str) -> MusicFile {
     let tag = Tag::read_from_path(path).unwrap();
+    let file_name = Path::new(path)
+        .file_stem()
+        .and_then(|name| name.to_str())
+        .unwrap_or("Unknown");
+
     MusicFile {
         path: path.to_string(),
-        title: tag.title().unwrap_or("Unknown").to_string(),
+        title: tag.title().unwrap_or(file_name).to_string(),
         artist: tag.artist().unwrap_or("Unknown").to_string(),
         album: tag.album().unwrap_or("Unknown").to_string(),
         genre: tag.genre().unwrap_or("Unknown").to_string(),
