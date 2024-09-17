b591aa58ddafa197e12f26cc4aad1f2e1cc50525
Romeo923 <cromeo5112@gmail.com>
2024-09-17 00:27:24 -0400

    Added pause, resume, and stop. Play spawns threads

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,63 @@
-mod cli;
 mod metadata;
 mod player;
 mod scanner;
 
 use home::home_dir;
+use player::Player;
 
 fn main() {
     let home = home_dir().expect("Unable ot find home directory");
     let lib_file = home.join(".local/share/music-lib-manager/music_library.json");
-
     let lib_file = lib_file.to_str().unwrap();
 
-    let args = cli::parse_args();
-    match args.command {
-        cli::Command::Scan { directory } => {
-            println!("Scanning {directory}...");
-            scanner::scan_directory(directory, lib_file)
+    let mut my_player = Player::new();
+
+    loop {
+        println!("\nEnter command:\n  scan <dir>\n  list\n  play <file_path>\n  pause\n  resume\n  stop\n  exit | quit | q");
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let mut words = input.trim().split_whitespace();
+        let command = words.next();
+        let argument = words.next();
+
+        match command {
+            Some("scan") => {
+                if let Some(dir) = argument {
+                    scanner::scan_directory(dir.to_string(), lib_file);
+                }
+            }
+            Some("list") => {
+                scanner::list_songs(lib_file);
+            }
+            Some("play") => {
+                if let Some(file_path) = argument {
+                    my_player.play(file_path.to_string());
+                }
+            }
+            Some("pause") => {
+                my_player.pause();
+            }
+            Some("resume") => {
+                my_player.resume();
+            }
+            Some("stop") => {
+                my_player.stop();
+            }
+            Some("exit") => {
+                return;
+            }
+            Some("quit") => {
+                return;
+            }
+            Some("q") => {
+                return;
+            }
+            _ => {
+                println!("Invalid Command");
+            }
         }
-        cli::Command::List => scanner::list_songs(lib_file),
-        cli::Command::Play { file } => player::play_song(file),
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,12 +1,87 @@
 use rodio::{Decoder, OutputStream, Sink};
 use std::fs::File;
 use std::io::BufReader;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-pub fn play_song(path: String) {
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
-    let file = File::open(path).unwrap();
-    let source = Decoder::new(BufReader::new(file)).unwrap();
-    sink.append(source);
-    sink.sleep_until_end();
+#[derive(PartialEq, Eq)]
+pub enum PlayerState {
+    Stopped,
+    Paused,
+    Playing,
+}
+
+pub struct Player {
+    state: PlayerState,
+    sink: Arc<Mutex<Option<Sink>>>,
+    stream: Option<OutputStream>,
+}
+
+impl Player {
+    pub fn new() -> Player {
+        Player {
+            state: PlayerState::Stopped,
+            sink: Arc::new(Mutex::new(None)),
+            stream: None,
+        }
+    }
+
+    pub fn play(&mut self, path: String) {
+        if self.state != PlayerState::Stopped {
+            return;
+        }
+
+        let sink = Arc::clone(&self.sink);
+        let (stream, stream_handle) = OutputStream::try_default().unwrap();
+
+        thread::spawn(move || {
+            let file = File::open(path).unwrap();
+            let source = Decoder::new(BufReader::new(file)).unwrap();
+
+            let new_sink = Sink::try_new(&stream_handle).unwrap();
+            new_sink.append(source);
+
+            *sink.lock().unwrap() = Some(new_sink);
+        });
+
+        self.stream = Some(stream);
+        self.state = PlayerState::Playing;
+    }
+
+    pub fn pause(&mut self) {
+        if self.state != PlayerState::Playing {
+            return;
+        }
+
+        if let Some(ref sink) = *self.sink.lock().unwrap() {
+            sink.pause();
+        }
+
+        self.state = PlayerState::Paused;
+    }
+
+    pub fn resume(&mut self) {
+        if self.state != PlayerState::Paused {
+            return;
+        }
+
+        if let Some(ref sink) = *self.sink.lock().unwrap() {
+            sink.play();
+        }
+
+        self.state = PlayerState::Playing;
+    }
+
+    pub fn stop(&mut self) {
+        if self.state == PlayerState::Stopped {
+            return;
+        }
+
+        if let Some(ref sink) = *self.sink.lock().unwrap() {
+            sink.stop();
+        }
+
+        self.stream = None;
+        self.state = PlayerState::Stopped;
+    }
 }
